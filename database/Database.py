d7cf90aeec01b5241a3b8f2f42680b137ffc1a3f
MineHaHaTa <minehahata@gmail.com>
2017-07-17 16:55:12 +0800

    renew the database's ports

--- database/Database.py
+++ database/Database.py
@@ -1,45 +1,67 @@
-
+import MySQLdb
+index = 0.03829
 
 def databaseInit():
     #database init
+    conn = MySQLdb.connect(host = '127.0.0.1', port = 3306, user = 'Demo-SystemAdmin', passwd = '666666', db = 'Demo-SystemDatabase', charset = 'utf8')
+    cur = conn.cursor()
+    conn.commit()
+    cur.close()
+    conn.close()
     pass
 
-def databaseFetch(img,name,occupation):
-    ''' Fetch the information in a database
-        input:  (list)img------the photo of this person
-                (str)name-----the name of the person (empty list possible)
-                (str)occuapation-----the job of the person (empty list possible)
-        output: (dict) infomation ------personal information (name, pic, ....), empty dict if existing is False
-                (list) hisInformation -----[last visit time:str, recent visit fequency]
-                (bool) existing ------whether the information of this person is in this database
+def databaseAppend(tempList):
     '''
+    send the temporary list to the database once an hour.
+    tempList{'Valid':True/False, 'data':[ {} {} … ]}
+    list{'time':20170101170506, 'feature':[ Normalized Eigenvalue ]}
+    return:None
+    '''
+    conn = MySQLdb.connect(host = '127.0.0.1', port = 3306, user = 'Demo-SystemAdmin', passwd = '666666', db = 'Demo-SystemDatabase', charset = 'utf8')
+    cur = conn.cursor()
+    conn.commit()
+    cur.close()
+    conn.close()
     pass
 
-def databaseAppend(personalInformation,date):
+def databaseSearch(feature):
     '''
-    This function is used when the system gets the information from a NEW person
-    :param personalInformation: (dict) {'img':list,'name':str,'occupation':str,''}
-    :param date: (str) the date of the image fetch
-    :return: None
+    search for information about the feature when the system gives a request.
+    feature(the normalized eigenvalue):numpy.ndarray
+    return:{'name':<str>, 'famiPeople':[{'name':<str>, 'photoAdd':<str>},{},{}], 'recentVisit':[int,int,int], 'firstVisit':int, 'pageAdd':<str>, 'photoAdd':<str>}
+    if there's nothing about the feature, then try a null dict.
     '''
+    conn = MySQLdb.connect(host = '127.0.0.1', port = 3306, user = 'Demo-SystemAdmin', passwd = '666666', db = 'Demo-SystemDatabase', charset = 'utf8')
+    cur = conn.cursor()
+    conn.commit()
+    cur.close()
+    conn.close()
     pass
 
-def databaseSearch():
+def databaseRenew(informationDict):
     '''
-    During free time, intend to renew the information or fill the blanks in the system
-    Need to deal with the frequency here(possibly one's information is never fetched on a website)
-    :return: (dict)infomation: personal information (name, pic, ....),the blank in the dict need to be filled
-                                or the information of a person that has not been renewed for a long time
+    renew the information in the database.
+    informationDict{(the renewed information needs to be wrote in the database), 'feature':[ Normalized Eigenvalue ]}
+    return:None
     '''
-
-def databaseRenew(personalInformation):
-    '''
-    renew the information in a database
-    :param personalInformation(dict): the newest information needs to write in the database
-    :return:None
-    '''
+    conn = MySQLdb.connect(host = '127.0.0.1', port = 3306, user = 'Demo-SystemAdmin', passwd = '666666', db = 'Demo-SystemDatabase', charset = 'utf8')
+    cur = conn.cursor()
+    conn.commit()
+    cur.close()
+    conn.close()
     pass
 
+def databaseRequest():
+    '''
+    provide a request for the crawler to find new information.
+    return:informationDict{(the information needs to be renewed is null), 'feature':[ Normalized Eigenvalue ]}
+    '''
+    conn = MySQLdb.connect(host = '127.0.0.1', port = 3306, user = 'Demo-SystemAdmin', passwd = '666666', db = 'Demo-SystemDatabase', charset = 'utf8')
+    cur = conn.cursor()
+    conn.commit()
+    cur.close()
+    conn.close()
+    pass
 
 if __name__=="__main__":
     databaseInit()
