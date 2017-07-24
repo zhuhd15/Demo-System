384ac880ea711118c1624ce69dfa6938cc911105
rexlin1010 <rexlin1010.2319150605@gmail.com>
2017-07-24 18:49:12 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -58,6 +58,7 @@ def get_info(url, info):
     addr = str(addr).replace('[', '')
     addr = str(addr).replace(']', '')
     addr = str(addr).replace('\'', '')
+    addr = str(addr).replace('</p><p>', ' ')
     # print(addr)
     # form_tel = re.compile('[1084]?\s?</p>\s?<p>\s?(电话.+?)\s?<')
     form_tel = re.compile('(电话[^/<>。]+?[0-9].+?)\s?[<，]')
@@ -74,37 +75,40 @@ def get_info(url, info):
     fax = str(fax).replace(']', '')
     fax = str(fax).replace('\'', '')
     # print(fax)
-    form_email = re.compile('(电?子?邮.+?tsinghua[^<>]+?)\s?</')
-    email = form_email.findall(html)
-    #print(email)
+    form_email0 = re.compile('(电?子?邮[^/]+?tsinghua[^<>]+?)\s?</')          #中文
+    form_email1 = re.compile('([eE]?-?mail.{2,50}tsinghua[^<>]+?)\s?</')        #english version
+    email0 = form_email0.findall(html)
+    email1 = form_email1.findall(html)
+    email = email0 + email1
     if name=='':
         email = ''
-    #print(email)
-    try:
-        email = email[email.index('href')]
-    except:
-        pass
-    #print(email)
-    if 'href' in str(email):
-        form_email2 = re.compile('"mailto:(.+?@.+?)"')
-        email2 = form_email2.findall(str(email))
-        email = '电子邮箱：' + str(email2)  # 超鏈接email....
-    email = str(email).replace('AT', '@')
-    email = str(email).replace('(at)', '@')
-    email = str(email).replace('[at]', '@')
-    email = str(email).replace(' ', '')
-    email = str(email).replace('[', '')
-    email = str(email).replace(']', '')
-    email = str(email).replace('\'', '')
-    email = str(email).replace('</font>', '')
-    email = str(email).replace('<fontsize="2">', '')
+    else:
+        if len(email)>1:
+            for x in range(0,len(email)-1):
+                if 'mailto' in str(email[x]):
+                    email = email[x]
+        email = str(email).replace(' AT ', '@')
+        email = str(email).replace('(at)', '@')
+        email = str(email).replace('[at]', '@')
+        email = str(email).replace('[dot]', '.')
+        email = str(email).replace(' ', '')
+        if 'mailto' in str(email):
+            form_email2 = re.compile('mailto:(.+?@.+?)"')
+            email2 = form_email2.findall(str(email))
+            email = email2[0]  # 超鏈接email
+        else:
+            form_email3 = re.compile('([a-zA-Z0-9\-_*]+?@.+)\']')
+            email3 = form_email3.findall(str(email))
+            email = email3
+        email = str(email).replace('[', '')
+        email = str(email).replace(']', '')
+        email = str(email).replace('\'', '')
     #print(email)
     form_page = re.compile('主页.+?\s?.+?\s?.+?href="(.+?)"')
     page = form_page.findall(html)
     page = str(page).replace('[', '')
     page = str(page).replace(']', '')
     # print(page)
-    # form_academic1 = re.compile('学术成果.+?(\[1][^<>[]+)',re.DOTALL)
     form_academic1 = re.compile('学术成果.+?(\[?1[\.\]、][^<>[]+)', re.DOTALL)
     academic1 = form_academic1.findall(html)
     # print(academic1)
@@ -120,35 +124,49 @@ def get_info(url, info):
 
     return info
 
-
-
-def get_Img(start_url, url, infos,detectionModel,recognitionModel):
+def find_imglist(url):
     html = get_Html(url)
     reg = r'src="(.+?\.[jJpP][pPnN][gG])"'
     imgre = re.compile(reg)
     html = html.decode('utf-8', 'ignore')  # python3
     img_list = imgre.findall(html)
+    return img_list
+
+def store_pic(img_url, info):
+    # create new folder
+    path = "d:\Demo-System\Spider"
+    title = time.strftime("%Y_%m_%d", time.localtime())
+    new_path = os.path.join(path, title)
+    if not os.path.isdir(new_path):
+        os.makedirs(new_path)
+    if 'png' in img_url or 'PNG' in img_url:
+        pic_path = os.path.join(new_path, currentTime() + '.png')
+    elif 'jpg' in img_url or 'JPG' in img_url:
+        pic_path = os.path.join(new_path, currentTime() + '.jpg')
+    elif 'bmp' in img_url or 'BMP' in img_url:
+        pic_path = os.path.join(new_path, currentTime() + '.bmp')
+    try:
+        urllib.request.urlretrieve(img_url, pic_path)  #store in the new folder
+    except:
+        pass            #404 Not Found
+    info.update(img_path=pic_path)
+
+
+def get_img_by_ramdonrun(start_url, url, detectionModel, recognitionModel):
+    img_list = find_imglist(url)
+    info = {}
 
     for img_url in img_list:
         info = {}
         if 'http' not in img_url:
             img_url = start_url + img_url
-
-
+            
         info = get_info(url, info)
+        info.update(img_url=img_url, url=url)
+        store_pic(img_url, info)            #save the picture to local
 
-        if info['name'] != '':
-            # create new folder
-            path = "/home/luka/PycharmProjects/Github/Spider/Spider/"
-            title = time.strftime("%Y_%m_%d", time.localtime())
-            new_path = os.path.join(path, title)
-            if not os.path.isdir(new_path):
-                os.makedirs(new_path)
-            pic_path = os.path.join(new_path, currentTime() + '.jpg')
-            #urllib.request.urlretrieve(img_url, pic_path)  # store in the new folder
-
-            info.update(img_url=img_url, url=url, img_path=pic_path)
-            img = Image.open(io.BytesIO(urllib.request.urlopen(img_url).read()))
+        try:
+            img = Image.open(io.BytesIO(urllib.request.urlopen(img_url, timeout=2).read()))
             img = numpy.array(img)
             # Convert RGB to BGR
             img = img[:, :, ::-1].copy()
@@ -181,7 +199,7 @@ def get_Img(start_url, url, infos,detectionModel,recognitionModel):
             [bboxset, extend] = FaceDetect_spider(image2, 50, detectionModel)
             info['feature'] = []
             if len(bboxset) == 0:
-                return None
+                yield None
 
             cv2.imwrite(pic_path,img)
             for bbox in bboxset:
@@ -193,40 +211,13 @@ def get_Img(start_url, url, infos,detectionModel,recognitionModel):
                 info['address'] = info['address'][0]
             if (len(info['feature']) == 1 and (info['name'] != '[]' or info['academic'] != [])) or len(
                         info['feature']) > 1:
-                infos.append(info)
-
-#    print(len(infos))
-    print("Spider is working...")
-    time.sleep(0.1)
-    return infos
-
-
-def search_in_department(start_url, infos,detectionModel,recognitionModel):
-    queue = deque()
-    visited = set()
-    queue.append(start_url)
-    cnt = 0
-    new_infos = infos
-    while queue:
-        url = queue.popleft()
-        visited |= {url}
-        cnt += 1
-        try:
-            urlop = urllib.request.urlopen(url, timeout=2)
-            data = urlop.read().decode('utf-8')
-            html = get_Html(url).decode('utf-8', 'ignore')
-            new_infos = get_Img(start_url, url, infos,detectionModel,recognitionModel)
+                yield info      
         except:
             continue
-
-        linkre = re.compile('href="(.+?\.html)"')
-        for x in linkre.findall(data):
-            if 'http' not in x:
-                x = start_url + x
-            if 'eeen' not in x and 'sina' not in x and 'youku' not in x and x not in visited and x not in queue:
-                queue.append(x)
-    return new_infos
-
+         
+        print("Spider is working...")
+        time.sleep(0.1)
+        yield info  
 
 def Spider(caffemodel):
     '''caffe.set_mode_gpu()
@@ -244,10 +235,35 @@ def Spider(caffemodel):
     detectionModel=caffemodel[0]
     recognitionModel=caffemodel[1]
     initial_url = 'http://www.ee.tsinghua.edu.cn'  # ee page
-    info = {}
-    infos = [info]
-    new_infos = search_in_department(initial_url, infos,detectionModel,recognitionModel)
-    return new_infos
+    #info = {}
+    #infos = [info]
+    #new_infos = search_in_department(initial_url, infos,detectionModel,recognitionModel)
+    
+    queue = deque()
+    visited = set()
+    queue.append(start_url)
+    #new_infos = infos
+    while queue:
+        url = queue.popleft()
+        visited |= {url}
+        try:
+            urlop = urllib.request.urlopen(url, timeout=2)
+            data = urlop.read().decode('utf-8')
+            #new_infos = get_img_by_ramdonrun(start_url, url, infos,detectionModel,recognitionModel)
+        except:
+            continue
+
+        linkre = re.compile('href="(.+?\.html)"')
+        for x in linkre.findall(data):
+            if 'http' not in x:
+                x = start_url + x
+            if 'eeen' not in x and 'tsinghua' in x and x not in visited and x not in queue:  # block english vision of the ee page and non-tsinghua websites
+                queue.append(x)
+                
+        for j in get_img_by_ramdonrun(initial_url, url, detectionModel, recognitionModel):
+            yield j
+    #return new_infos
+    
 
 def tss():
     caffe.set_mode_gpu()
@@ -269,6 +285,8 @@ def tss():
 if __name__ == '__main__':
     tp=multiprocessing.Process(target=tss)
     ts=multiprocessing.Process(target=Spider)
+    #for i in spider(1):
+    #    print(i)       #不知道怎么写成multiprocessing的表示法，我测试的时候只要用for循环就会一条一条不断输出，直到跑完一遍为止。#输出的是i
     tp.start()
     ts.start()
     while(1):
